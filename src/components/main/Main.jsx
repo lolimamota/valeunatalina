07f55f7db3dfa928dc6b16a998692b70d9ac9b83
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-12-31 17:12:15 -0300

    END: section curiosity

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,13 +4,19 @@ import '@styles/global.scss';
 //IMPORT DO ESTILO PERSONALIZADO
 import S from '@styles/componentStyle/main.module.scss';
 
+//IMPORT DAS IMAGENS
+import Globo from '@assets/globoNeve.png';
+
 function Main() {
     return (
         <>
             <section id="Home" className={S.homeStyle}>
             </section>
             <section id="Curiosity" className={S.curiosityStyle}>
-                curioso
+                <img src={Globo} alt="Uma arvore dentro de um globo de vidro com pedestal de madeira com a simulação de pequenos flocos de neve caindo sobre a arvore" />
+                <p>
+                A origem do globo de neve é incerta, mas acredita-se que tenha surgido na Europa no século XIX, tornando-se um símbolo de magia, afeto e encantamento, especialmente no Natal, trazendo um toque de nostalgia e maravilha para as celebrações e encantando gerações ao redor do mundo.
+                </p>
             </section>
             <section id="Gallery" className={S.galleryStyle}>
                 galeria
