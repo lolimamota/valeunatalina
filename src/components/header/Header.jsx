aa2204af7d4caccf0e001a758281ba52354e6a24
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-12-31 10:11:54 -0300

    ADD: estilos da header e sections

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,14 +12,13 @@ import Arvore from '@assets/arvoreLogo.png';
 function Header() {
   return (
     <header className={S.headerStyle}>
-      <section>
+      <section className={S.sectionLogo}>
         <img src={Arvore} alt="Arvore de natal verde com presemtes embaixo e bolinhas vermelhas e amarelas com uma estrela na ponta" />
-        
         <h1>
           Feliz Natal !!!
         </h1>
       </section>
-      <section>
+      <section className={S.sectionLogo}>
         < RouteFunction />
       </section>
     </header>
