f038b740345128c5eb84788537c97696c3f68eb4
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-12-31 14:54:54 -0300

    Finalização das funções para o clique e disposição do espaço.

--- src/components/routes/RouteFunction.jsx
+++ src/components/routes/RouteFunction.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+//PRINCIPAIS IMPORTS
+import React from 'react';
+import '@styles/global.scss';
+
+//IMPORT DO ESTILO PERSONALIZADO
+import S from '@styles/componentStyle/route.module.scss';
 
 function routeFunction() {
+  const toSection = (sectionId) =>{
+    const element = document.getElementById(
+      sectionId
+    );
+    if (element){
+        ElementInternals.toSectionView(
+          {
+            behavior: 'smooth'
+          }
+        );
+    }
+  }
+
   return (
-    <div>routeFunction</div>
+    <section className={S.sectionNav}>
+      <ul className={S.sectionUl}>
+        <li onClick={() => toSection('Home')}>
+          Início
+        </li>
+        <li onClick={() => toSection('Curiosity')}>
+          Curiosidades
+        </li>
+        <li onClick={() => toSection('Gallery')}>
+          Galeria
+        </li>
+      </ul>
+    </section>
   )
 }
 
